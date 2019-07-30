11b1932fce5ae5fd6c6d7f75fc32c7e58ba47129
akkipatel191996 <47867200+akkipatel191996@users.noreply.github.com>
2019-07-30 13:33:03 -0400

    Update led.c

--- src/led.c
+++ src/led.c
@@ -23,7 +23,7 @@
 int main(void) {
 
 #if defined (__USE_LPCOPEN)
-    // Read clock settings and update SystemCoreClock variable
+    /// Read clock settings and update SystemCoreClock variable
     SystemCoreClockUpdate();
 #if !defined(NO_BOARD_LIB)
     // Set up and initialize all required blocks and
